4ef9f69f14186bfa88fc57ffdd4b4f7a2f175639
ericggul <57379903+ericggul@users.noreply.github.com>
2025-10-23 19:36:04 +0900

    Feat: WheelPicker Code Refactoring

---
+++ components/mobile/1/WheelPicker/index.js
@@ -0,0 +1,71 @@
+"use client";
+
+import styles from "./style.module.css";
+
+import { useMemo, useState } from "react";
+import { useWheelPicker } from "./useWheelPicker";
+
+const WheelPickerWrapper = ({ className, children }) => (
+  <div className={`${styles.root} ${className ?? ""}`} data-rwp-wrapper>
+    {children}
+  </div>
+);
+
+const WheelPicker = ({ classNames, ...wheelPickerProps }) => {
+  const {
+    refs: { containerRef, wheelItemsRef, highlightListRef },
+    measurements: { containerHeight, itemHeight },
+    highlightListStyle,
+    wheelItems,
+    highlightItems,
+  } = useWheelPicker(wheelPickerProps);
+
+  return (
+    <div ref={containerRef} data-rwp style={{ height: containerHeight }}>
+      <ul ref={wheelItemsRef} data-rwp-options>
+        {wheelItems.map(({ key, dataIndex, label, style }) => (
+          <li
+            key={key}
+            className={classNames?.optionItem}
+            data-slot="option-item"
+            data-rwp-option
+            data-index={dataIndex}
+            style={style}
+          >
+            {label}
+          </li>
+        ))}
+      </ul>
+
+      <div
+        className={classNames?.highlightWrapper}
+        data-rwp-highlight-wrapper
+        data-slot="highlight-wrapper"
+        style={{
+          height: itemHeight,
+          lineHeight: `${itemHeight}px`,
+        }}
+      >
+        <ul
+          ref={highlightListRef}
+          data-rwp-highlight-list
+          style={highlightListStyle}
+        >
+          {highlightItems.map(({ key, label, style }) => (
+            <li
+              key={key}
+              className={classNames?.highlightItem}
+              data-slot="highlight-item"
+              data-rwp-highlight-item
+              style={style}
+            >
+              {label}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export { WheelPicker, WheelPickerWrapper };
